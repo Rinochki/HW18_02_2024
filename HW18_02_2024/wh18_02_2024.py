63f385beb81b2c8edc4607b75302e179135e229c
Rinochki <fenixrina212@gmail.com>
2024-02-18 19:30:56 +0200

    Створила кнопку

--- HW18_02_2024/wh18_02_2024.py
+++ HW18_02_2024/wh18_02_2024.py
@@ -1,7 +1,28 @@
 from tkinter import *
-
 root = Tk()
-root.geometry("500x450")
 
+text = 'Мопти'
+text1 = 'Йопти'
+
+nom = 0
+
+def on_button_click(event):
+    global nom
+    nom += 1
+    root.title(f"Клацнуто: {nom}")
+
+
+
+
+
+
+w1 = root.winfo_screenwidth() # ширина екрану
+h1 = root.winfo_screenheight() # висота екрану
+root.geometry(f"500x500+{int(w1/2-250)}+{int(h1/2-250)}")
+
+
+button = Button(root, bg="Gray", text="Натисни мене!")
+button.pack()
+button.bind("<Button-1>", on_button_click)
 
 root.mainloop()
