6a0cc28dcfc619d2a122aed7802aa0fa1ad1f9d4
Rinochki <fenixrina212@gmail.com>
2024-02-18 19:44:53 +0200

    додала Label та закінчила роботу.

--- HW18_02_2024/wh18_02_2024.py
+++ HW18_02_2024/wh18_02_2024.py
@@ -1,8 +1,8 @@
 from tkinter import *
 root = Tk()
 
-text = 'Мопти'
-text1 = 'Йопти'
+text = '<3 дихайте <3 0_0'
+text1 = 'x_x не дихайте x_x'
 
 nom = 0
 
@@ -10,16 +10,18 @@ def on_button_click(event):
     global nom
     nom += 1
     root.title(f"Клацнуто: {nom}")
-
-
-
-
+    if label.cget("text") == text:
+        label.config(text=text1)
+    else:
+        label.config(text=text)
 
 
 w1 = root.winfo_screenwidth() # ширина екрану
 h1 = root.winfo_screenheight() # висота екрану
-root.geometry(f"500x500+{int(w1/2-250)}+{int(h1/2-250)}")
+root.geometry(f"200x200+{int(w1/2-100)}+{int(h1/2-100)}")
 
+label = Label(root, text=text)
+label.pack()
 
 button = Button(root, bg="Gray", text="Натисни мене!")
 button.pack()
